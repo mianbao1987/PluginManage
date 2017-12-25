b33a80f29553d1a57673b05e13ef0b84f6767d01
唐海安 <mianbao1987@gmail.com>
2017-12-25 11:30:11 +0800

    添加项目文件。

---
+++ PluginTest/PluginTest.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+#include "PluginInstance.h"
+#include "PluginManager.h"
+
+void main()
+{
+	PluginManager &pluManager = PluginManager::GetInstance();
+	int errCode;
+	PluginInstance *pInstance = pluManager.Load("PluginShape",errCode);
+	pInstance = pluManager.Load("PluginShape",errCode);
+
+	printf("%s,%d\n",pInstance->GetFileName().c_str(),errCode);
+
+	pluManager.UnLoad("PluginShape");
+	printf("%d\n",pluManager.GetAllPlugins().size());
+
+}
